d9e706a4de5569f7e43c8eb0aa63b0818fcc8a78
Erin Kinnen <erin.kinnen@outlook.com>
2017-04-08 15:24:51 -0500

    mistakes

--- server/app.js
+++ server/app.js
@@ -4,11 +4,17 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var router = require('./routes/index.js');
 var db = require('./modules/db.js');
-app.set('port', (process.env.PORT || 5000));
+
+app.set('port', (process.env.PORT || 2000));
 
 app.use(express.static('server/public'));
 
 app.use('/', router);
+
+
+
+
+
 
 app.listen(app.get('port'), function(){
   console.log("Listening on port: ", app.get('port'));
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,31 @@ console.log("index.js sourced");
 
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var path = require('path');
+var MovieSchema = mongoose.Schema({
+  title: String,
+  plot: String,
+  year: String
+});
+
+var Movie = mongoose.model('movie', MovieSchema, 'movie');
+
+router.get('/', function(req,res){
+
+});
+
+router.post('/', function(req,res){
+
+});
+
+router.put('/', function(req,res){
+
+});
+
+router.delete('/', function(req,res){
+
+});
 
 router.get('/', function(req,res){
   res.sendFile(path.resolve('server/public/views/index.html'));
