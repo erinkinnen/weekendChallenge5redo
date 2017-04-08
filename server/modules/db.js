dd56705e9a0aec09c9dd101a310a7e1dabe9b659
Erin Kinnen <erin.kinnen@outlook.com>
2017-04-08 14:07:57 -0500

    adds mongo connection

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,1 +1,15 @@
 console.log("db.js sourced");
+
+var mongoose = require('mongoose');
+var mongoURI = 'mongodb://localhost:27017/api_movies';
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on('error', function(error){
+  console.log('MongoDB connection error: ', error);
+});
+
+MongoDB.once('open', function(){
+  console.log("MongoDB connection open");
+});
+
+module.exports = MongoDB;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,1 +1,11 @@
 console.log("index.js sourced");
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req,res){
+  res.sendFile(path.resolve('server/public/views/index.html'));
+});
+
+
+module.exports = router;
